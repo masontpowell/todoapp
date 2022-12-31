fc168a27b7f8502782da6082af9119b97a4fe761
masontpowell <masontpowell11@gmail.com>
2022-12-31 00:44:27 -0600

    no idea what i am doing hehe

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-let number = document.getElementById('number');
+let number = document.getElementById("number");
 let counter = 0;
 
 setInterval(() => {
@@ -6,6 +6,31 @@ setInterval(() => {
         clearInterval;
     } else {
         counter += 1;
-        number.innerHTML = '${counter}%';
+        number.innerHTML = counter + "%";
     }
 }, 30);
+
+const dailyCheckboxes = Array.from({length: 456}, (x, i) => i);
+
+dailyCheckboxes.forEach((daily)=>{
+    //  generate id
+    const id = `daily-${daily}`;
+
+    // create a label
+    const label = document.createElement('label');
+    label.setAttribute("for", id);
+   
+    // create a checkbox
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.name = "dailyCheck";
+    checkbox.value = daily;
+    checkbox.id = id;
+
+    // place the checkbox inside a label
+    label.appendChild(checkbox);
+    // create text node
+    label.appendChild(document.createTextNode(daily));
+    // add the label to the root
+    document.querySelector("#root").appendChild(label);
+});
