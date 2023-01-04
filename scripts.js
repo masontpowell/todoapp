ea9f47432844922cf4f50b182e996e120bf29c16
masontpowell <108495335+masontpowell@users.noreply.github.com>
2023-01-04 01:22:05 -0600

    finally got most of everything to work properly

--- scripts.js
+++ scripts.js
@@ -1,32 +1,3 @@
-
-// CREATING ALL OF THE DAILY CHECKBOXES
-const dailyCheckboxes = Array.from({length: 456}, (x, i) => i);
-
-dailyCheckboxes.forEach((daily)=>{
-    //  generate id
-    const id = `daily-${daily}`;
-
-    // create a label
-    const label = document.createElement('label');
-    label.setAttribute("for", id);
-   
-    // create a checkbox
-    const checkbox = document.createElement('input');
-    checkbox.type = "checkbox";
-    checkbox.name = "dailyCheck";
-    checkbox.value = daily;
-    checkbox.id = id;
-    
-
-    // place the checkbox inside a label
-    label.appendChild(checkbox);
-    // create text node
-    label.appendChild(document.createTextNode(daily));
-    // add the label to the root
-    document.querySelector("#root").appendChild(label);
-});
-
-
 //CHECKING IF THE CHECKBOXES HAVE BEEN CHECKED OR UNCHECKED AND THEN RETURNING THE NUMBER
 //AS VALCHECKCOUNT
 var valCheckCount = 0;
@@ -43,19 +14,58 @@ checkboxCounter.forEach(function(checkboxD) {
        update();
     });
 });
-    
+
+
+//function to add a new row of inputs when the + is clicked.
+function addRowFunction() {
+    var parentTable = document.getElementById("table1Body");
+    var myTd,myInputCheck,myInputText,myLabelText;
+    var myTr = document.createElement('tr');
+    myTr.setAttribute('class' , 'unit-table');
+    for (var j =0; j < 1; j++) {
+        myLabelText = document.createElement('label');
+        myLabelText.setAttribute('class', 'form__label');
+        myLabelText.setAttribute('for', 'task');
+        myLabelText.innerHTML = "Task";
+        myTd = document.createElement('td');
+        myInputText = document.createElement('input');
+        myInputText.setAttribute('type','text');
+        myInputText.setAttribute('class', 'form__field');
+        myInputText.setAttribute('placeholder', 'task');
+        myTd.appendChild(myInputText);
+        myTd.appendChild(myLabelText);
+        myTr.appendChild(myTd);
+    }
+    for (var i = 0; i < 31; i++) {
+        myTd = document.createElement('td');
+        myInputCheck = document.createElement('input');
+        myInputCheck.setAttribute('type','checkbox');
+        myInputCheck.setAttribute('name' , 'dailyCheck');
+        myInputCheck.addEventListener('change', function(e) {
+            update();
+        });
+        myTd.appendChild(myInputCheck);
+        myTr.appendChild(myTd);
+    }
+    parentTable.appendChild(myTr);
+}
+
+
+
 //ALL OF THE CODE USED TO CREATE THE CIRCULAR PROGRESS BAR AND TAKES THE INPUT FROM
 //VALCHECKCOUNT AND THE FUNCTION ABOVE.
 let circularProgress = document.querySelector(".circular-progress"),
     progressValue = document.querySelector(".progress-value");
+
 
 let progressStartValue = 0,
     progressEndValue = valCheckCount,
     speed = 20;
 
 let progress = setInterval(() => {
+let progressLength = document.querySelectorAll('input[name="dailyCheck"]').length;
     progressStartValue++;
-let percentageAnswer = Math.round((valCheckCount / 456) * 100);
+let percentageAnswer = Math.round((valCheckCount / progressLength) * 100);
 
     progressValue.textContent = `${percentageAnswer}%`
     circularProgress.style.background = `conic-gradient(#7d2ae8 ${percentageAnswer * 3.6}deg, #ededed 0deg)`
@@ -64,19 +74,3 @@ let percentageAnswer = Math.round((valCheckCount / 456) * 100);
         clearInterval(progress);
     }
 }, speed);
-
-function addRowFunction() {
-    var table = document.getElementById("table1");
-    var row = table.insertRow(1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell1 = document.createElement("input");
-    cell1.type = "text";
-    cell1.name = "taskName";
-    cell1.value = "incomplete";
-    cell1.id = "task1";
-
-
-    cell2.innerHTML = "new row2";
-}
